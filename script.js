a130f48f36a28a2b37a03a02f1e52e2ebad16222
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-06-10 22:25:22 +0530

    Dino Lag Jump Fixed

--- script.js
+++ script.js
@@ -1,66 +1,69 @@
-score=0;
-cross=true;
+score = 0;
+cross = true;
 
 
-document.onkeydown=function(e){
-    console.log("Key Code is",e.keyCode);
-        if(e.keyCode==38){
-            dino=document.querySelector('.dino');
-            dino.classList.add('animateDino');
-            setTimeout(()=>{
-                dino.classList.remove('animateDino');
-            },700);
-        }
-
-
-if(e.keyCode==39){
-    dino=document.querySelector('.dino');
-    dinoX=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-    dino.style.left=dinoX+112+"px";   
+document.onkeydown = function (e) {
+    console.log("Key Code is", e.keyCode);
+    if (e.keyCode == 38) {
+        dino = document.querySelector('.dino');
+        dino.classList.add('animateDino');
+        setTimeout(() => {
+            dino.classList.remove('animateDino');
+        }, 700);
     }
 
 
-    if(e.keyCode==37){
-        dino=document.querySelector('.dino');
-        dinoX=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-        dino.style.left=(dinoX-112)+"px";   
+    if (e.keyCode == 39) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
+    }
+
+
+    if (e.keyCode == 37) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = (dinoX - 112) + "px";
     }
 }
 
 
-setInterval(()=>{
-    dino=document.querySelector('.dino')
-    gameOver=document.querySelector('.gameOver');
-    obstacle=document.querySelector('.obstacle');
+setInterval(() => {
+    dino = document.querySelector('.dino')
+    gameOver = document.querySelector('.gameOver');
+    obstacle = document.querySelector('.obstacle');
 
-    dx=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-    dy=parseInt(window.getComputedStyle(dino,null).getPropertyValue('top'));
-
-
-    ox=parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('left'));
-    oy=parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('top'));
-
-    offsetX=Math.abs(dx-ox);
-    offsetY=Math.abs(dy-oy);
-     if(offsetX<93 && offsetY<52){
-         gameOver.style.visibility='visible';
-         obstacle.classList.remove('obstacleAni');
-     }
-       
-        else if(offsetX<145 && cross){
-            score+=1;
-            updateScore(score);
-            cross=false;
-            setTimeout(()=>{
-                cross=true
-            },1000);
-        }
+    dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('top'));
 
 
-},100);
+    ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
+    oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'));
 
-function updateScore(score){
-    scoreCont.innerHTML="YOUR SCORE: " +score;
+    offsetX = Math.abs(dx - ox);
+    offsetY = Math.abs(dy - oy);
+    if (offsetX < 113 && offsetY < 52) {
+        gameOver.style.visibility = 'visible';
+        obstacle.classList.remove('obstacleAni');
+    }
+    else if (offsetX < 145 && cross) {
+        score += 1;
+        updateScore(score);
+        cross = false;
+        setTimeout(() => {
+            cross = true
+        }, 1000);
+        setTimeout(() => {
+            aniDur = parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue('animation-duration'));
+            newDur = aniDur - 0.1;
+            obstacle.style.animationDuration = newDur + 's';
+        }, 500);
+
+    }
+}, 10);
+
+function updateScore(score) {
+    scoreCont.innerHTML = "YOUR SCORE: " + score;
 }
 
 
